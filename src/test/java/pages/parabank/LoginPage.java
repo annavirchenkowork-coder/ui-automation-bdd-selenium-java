b2e5acbdc2d097ae1e72d2fce2033dab149acd86
annavirchenkowork-coder <anna.virchenko.work@gmail.com>
2025-09-27 18:24:15 -0400

    Add Parabank LoginPage with locators

---
+++ src/test/java/pages/parabank/LoginPage.java
@@ -0,0 +1,21 @@
+package pages.parabank;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import util.BrowserUtil;
+
+public class LoginPage {
+    private final WebDriver driver;
+
+    private static final By USERNAME = By.name("username");
+    private static final By PASSWORD = By.name("password");
+    private static final By BTN_LOGIN = By.cssSelector("input.button");
+
+    private static final By ERROR_MSG = By.cssSelector("p.error");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    
+}
