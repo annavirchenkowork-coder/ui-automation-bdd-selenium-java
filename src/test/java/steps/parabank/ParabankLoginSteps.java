a6ac0a6e694626abcd42be3ef235a5b758108768
annavirchenkowork-coder <anna.virchenko.work@gmail.com>
2025-09-28 07:43:53 -0400

    feat(steps): implement ParabankLoginSteps with open, login, and overview verification

--- src/test/java/steps/parabank/ParabankLoginSteps.java
+++ src/test/java/steps/parabank/ParabankLoginSteps.java
@@ -1,6 +1,7 @@
 package steps.parabank;
 
 import io.cucumber.java.en.*;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import pages.parabank.*;
 import util.Driver;
@@ -13,20 +14,17 @@ public class ParabankLoginSteps {
 
     @Given("the user is on the Parabank login page")
     public void open_login_page() {
-
+        loginPage.open();
     }
 
     @When("the user logs into Parabank with username {string} and password {string}")
     public void login_with_credentials(String user, String pass) {
-
+        loginPage.login(user, pass);
     }
 
     @Then("the Accounts Overview page should be visible")
     public void verify_accounts_overview() {
-
+        Assertions.assertTrue(accountsPage.isVisible(), "Accounts Overview page is not visible.");
     }
 
-    @Then("an error message should be displayed")
-    public void verify_error_message() {
-    }
 }
