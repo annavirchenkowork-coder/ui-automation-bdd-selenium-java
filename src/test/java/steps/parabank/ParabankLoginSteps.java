9f4d4b4f14e4c90594b6e70844b42030a0105e30
annavirchenkowork-coder <anna.virchenko.work@gmail.com>
2025-09-28 07:52:00 -0400

    feat(steps): add steps for direct Accounts Overview access and login page visibility

--- src/test/java/steps/parabank/ParabankLoginSteps.java
+++ src/test/java/steps/parabank/ParabankLoginSteps.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.*;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import pages.parabank.*;
+import util.ConfigurationReader;
 import util.Driver;
 
 public class ParabankLoginSteps {
@@ -26,5 +27,16 @@ public class ParabankLoginSteps {
     public void verify_accounts_overview() {
         Assertions.assertTrue(accountsPage.isVisible(), "Accounts Overview page is not visible.");
     }
+
+    @Given("the user tries to access the Accounts Overview page directly")
+    public void access_overview_directly() {
+        driver.get(ConfigurationReader.getProperty("baseUrl.parabank") + "/parabank/overview.htm");
+    }
+
+    @Then("the Parabank login page should be visible")
+    public void verify_login_page_visible() {
+        Assertions.assertTrue(loginPage.isVisible(), "Parabank login page is not visible.");
+    }
+
 
 }
