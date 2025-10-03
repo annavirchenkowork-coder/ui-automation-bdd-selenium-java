1997ce1bedc628fb6ae4220a02900c656c2969c6
annavirchenkowork-coder <anna.virchenko.work@gmail.com>
2025-10-03 16:44:56 -0400

    feat(AccountsOverviewPage): add locators for account links and 'Open New Account'

--- src/test/java/pages/parabank/AccountsOverviewPage.java
+++ src/test/java/pages/parabank/AccountsOverviewPage.java
@@ -5,6 +5,8 @@ import util.BrowserUtil;
 
 public class AccountsOverviewPage {
     private static final By ROOT = By.cssSelector("h1.title");
+    private static final By ACCOUNT_LINKS = By.cssSelector("#accountTable tbody tr td a");
+    private static final By LEFT_OPEN_NEW_ACCOUNT = By.linkText("Open New Account");
 
     public AccountsOverviewPage() {
     }
