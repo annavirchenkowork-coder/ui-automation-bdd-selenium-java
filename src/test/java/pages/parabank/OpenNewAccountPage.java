4fc1d16eafa42f701961840aca6b0bc57be7fb4b
annavirchenkowork-coder <anna.virchenko.work@gmail.com>
2025-10-03 17:02:41 -0400

    feat(parabank): add OpenNewAccountPage with initial locators

---
+++ src/test/java/pages/parabank/OpenNewAccountPage.java
@@ -0,0 +1,11 @@
+package pages.parabank;
+
+import org.openqa.selenium.By;
+
+public class OpenNewAccountPage {
+    private static final By TYPE = By.id("type");
+    private static final By FROM = By.id("fromAccountId");
+    private static final By BTN_OPEN = By.cssSelector("input.button");
+    private static final By NEW_ACCOUNT_ID = By.id("newAccountId");
+
+}
